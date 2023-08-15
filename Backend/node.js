b96f88e527fedf2ca5afd0557c15b78743b33122
KHDonmoyer <kdonm7@gmail.com>
2023-08-14 20:56:32 -0700

    added beggining of app use and back end intergration for username and passwords

--- Backend/node.js
+++ Backend/node.js
@@ -10,3 +10,27 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+
+app.use(cors({credentials:true,origin:'http//localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+mongoose.connect('mongodb+srv://blog:RD8paskYC8Ayj09u@cluster0.pflplid.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt,hashSync(password,salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
+});
